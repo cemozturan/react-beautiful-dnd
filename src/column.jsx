1554de6fd842d02d353e991e422915041a2c9e33
Cem Ozturan <cemozturan@gmail.com>
2018-08-07 17:08:24 +0100

    Added the drag and drop functionality using react-beautiful-dnd (reordering via drag and drop doesn't work yet)

--- src/column.jsx
+++ src/column.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Droppable } from 'react-beautiful-dnd';
 import Task from './task';
 
 const Container = styled.div`
@@ -19,10 +20,26 @@ export default class Column extends React.Component {
     return (
         <Container>
           <Title>{this.props.column.title}</Title>
-          <TaskList>
-            {this.props.tasks.map(task => <Task key={task.id} task={task} />)}
-          </TaskList>
+          <Droppable droppableId={this.props.columnId}>
+            {(provided) => (
+              <TaskList
+                innerRef={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                {this.props.tasks.map((task, index) => (
+                  <Task key={task.id} task={task} index={index}/>
+                ))}
+                {provided.placeholder}
+              </TaskList>
+            )}
+          </Droppable>
         </Container>
     )
   }
 }
+
+// Droppable expects its child to be a function!
+// The first argument is 'provided', which is an object.
+// Its 'droppableProps' needs to be passed in as a prop to the droppable component. You can pass them in individually as well, but we won't do it here.
+// innerRef is needed to pass the DOM node of our component to react-beautiful-dnd.
+// placeholder is used to increase the space in a droppable component during a drag if it is needed.
--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@atlaskit/css-reset';
+import { DragDropContext } from 'react-beautiful-dnd';
 import initialData from './initial-data';
 import Column from './column';
 
 class App extends React.Component {
   state = initialData;
 
+  onDragEnd = (result) => {
+    // TODO: reorder the column
+  };
+
   render() {
-    return this.state.columnOrder.map((columnId) => {
-      const column = this.state.columns[columnId];
-      const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
-      return <Column key={column.id} column={column} tasks={tasks} />
-    });
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map((columnId) => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </DragDropContext>
+    );
   }
 }
 
